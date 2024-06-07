e64f1a362d41cec10bd75833c68169e51bd04790
ronaldophc <129230604+ronaldophc@users.noreply.github.com>
2024-06-07 08:57:20 -0300

    SEO optimization

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function setLanguage(lang) {
         elem.classList.add('active');
     });
 }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }).catch(error => {
+        console.log('Service Worker registration failed:', error);
+      });
+  }
+  
